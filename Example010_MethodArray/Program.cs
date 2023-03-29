693b01484da6c244c761daa840e017324da050c7
Irmica <128592858+Irmica@users.noreply.github.com>
2023-03-29 22:28:26 +0300

    Добавлен 10 пример Поиск числа в массиве

---
+++ Example010_MethodArray/Program.cs
@@ -0,0 +1,13 @@
+﻿int[] array = { 15, 24, 32, 14, 53, 46, 75, 88 };
+int n = array.Length;
+int find = 53;
+int index = 0;
+while (index < n)
+{
+    if (array[index] == find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+    index +=1;
+}
