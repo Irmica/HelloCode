f19491abea6839a5860215e584af1fd4b3f8a080
Irmica <128592858+Irmica@users.noreply.github.com>
2023-03-29 21:30:23 +0300

    Добавлен в 8 пример метод

--- Example008_IntroMethod/Program.cs
+++ Example008_IntroMethod/Program.cs
@@ -15,16 +15,15 @@ int a3 = 13;
 int b3 = 23;
 int c3 = 33;
 
-int max = a1;
-if (b1>max) max = b1;
-if (b2>max) max = b2;
-if (b3>max) max = b3;
+// int max1 = Max(a1, b1,c1);
+// int max2 = Max(a2, b2, c2);
+// int max3 = Max(a3, b3,c3);
 
-if (a2>max) max = a2;
-if (a3>max) max = a3;
+// int max = Max(max1, max2, max3);
 
-if (c1>max) max = c1;
-if (c2>max) max = c2;
-if (c3>max) max = c3;
+int max = Max(
+    Max(a1, b1, c1),
+    Max(a2, b2, c2), 
+    Max(a3, b3, c3));
 
 Console.WriteLine(max);
